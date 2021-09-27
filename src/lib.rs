c9a30a2bb559414259b76388b68a0849951a1b9e
Alice <alice.i.cecile@gmail.com>
2021-09-27 02:53:09 -0400

    Example app runs successfully!

--- src/lib.rs
+++ src/lib.rs
@@ -6,20 +6,10 @@ pub struct HelloWorldPlugin;
 
 impl Plugin for HelloWorldPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_startup_system(hello_world.system())
-            .add_system(count_up.system());
+        app.add_startup_system(hello_world.system());
     }
 }
 
 fn hello_world() {
     println!("Hello, World!");
-}
-
-#[derive(Default)]
-struct Counter(u64);
-
-fn count_up(mut counter: Local<Counter>) {
-    counter.0 += 1;
-
-    println!("{}", counter.0);
 }
